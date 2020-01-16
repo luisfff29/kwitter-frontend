404d5842183e4c25d72b3361d2f6a46d924b90cc
luisfff29 <luisfff29@gmail.com>
2020-01-16 12:57:06 -0600

    for grading project

--- src/redux/stateReducers/users/index.js
+++ src/redux/stateReducers/users/index.js
@@ -1,3 +1,4 @@
 export { default as getUser } from "./getUser";
 export { default as getListOfUsers } from "./getListOfUsers";
 export { default as createNewUser } from "./createNewUser";
+export { default as updateUser } from "./updateUser";
