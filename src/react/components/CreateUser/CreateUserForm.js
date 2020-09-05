87de31c0f0ba00ced8a69a907c13167d2e9f7580
luisfff29 <luisfff29@gmail.com>
2020-09-05 06:09:42 -0500

    update create user page

--- src/react/components/CreateUser/CreateUserForm.js
+++ src/react/components/CreateUser/CreateUserForm.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import { Link, Spinner } from "../../components";
 import { withAsyncAction } from "../../HOCs";
-import { Button, Form, Message, Icon, Image } from "../../components";
-import BackgroundDarkMode from "../../assets/images/backgroundDarkMode.jpg";
+import { Button, Form, Message, Icon, Grid, Container } from "../../components";
+import "./CreateUserForm.css";
 
 class CreateUserForm extends Component {
   state = { username: "", displayName: "", password: "" };
@@ -23,64 +23,64 @@ class CreateUserForm extends Component {
     const { loading, error } = this.props;
 
     return (
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          marginTop: "-80px",
-        }}
-      >
-        <Image size="large" src={BackgroundDarkMode} />
-        <div>
-          <Message
-            attached
-            header="Welcome to Kwitter!"
-            content="Fill out the form below to sign-up for a new account"
-          />
-          <Form className="attached fluid segment">
-            <Form.Field>
-              <label>Username</label>
-              <input
-                type="text"
-                name="username"
-                placeholder="Username"
-                onChange={this.handleChange}
-                required
-              />
-            </Form.Field>
-            <Form.Field>
-              <label>Display Name</label>
-              <input
-                type="text"
-                name="displayName"
-                placeholder="Display Name"
-                onChange={this.handleChange}
-                required
-              />
-            </Form.Field>
-            <Form.Field>
-              <label>Password</label>
-              <input
-                type="password"
-                name="password"
-                placeholder="Password"
-                onChange={this.handleChange}
-                required
-              />
-            </Form.Field>
-            <Button color="blue" type="submit" onClick={this.handleCreateUser}>
-              Submit
-            </Button>
-          </Form>
-          {loading && <Spinner name="circle" color="white" />}
-          {error && <p style={{ color: "red" }}>{error.message}</p>}
-          <Message attached="bottom" warning>
-            <Icon name="help" />
-            Already signed up?&nbsp;<Link to="/">Login here</Link>
-            &nbsp;instead.
-          </Message>
-        </div>
-      </div>
+      <Container>
+        <Grid stackable columns={2}>
+          <Grid.Column width={4} className="BackgroundSignup"></Grid.Column>
+          <Grid.Column width={12}>
+            <Message
+              attached
+              header="Welcome to Kwitter!"
+              content="Fill out the form below to sign-up for a new account"
+            />
+            <Form className="attached fluid segment">
+              <Form.Field>
+                <label>Username</label>
+                <input
+                  type="text"
+                  name="username"
+                  placeholder="Username"
+                  onChange={this.handleChange}
+                  required
+                />
+              </Form.Field>
+              <Form.Field>
+                <label>Display Name</label>
+                <input
+                  type="text"
+                  name="displayName"
+                  placeholder="Display Name"
+                  onChange={this.handleChange}
+                  required
+                />
+              </Form.Field>
+              <Form.Field>
+                <label>Password</label>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Password"
+                  onChange={this.handleChange}
+                  required
+                />
+              </Form.Field>
+              <Button
+                color="blue"
+                type="submit"
+                onClick={this.handleCreateUser}
+              >
+                Submit
+              </Button>
+            </Form>
+            {loading && <Spinner name="circle" color="white" />}
+            {error && <p style={{ color: "red" }}>{error.message}</p>}
+            <Message attached="bottom" warning>
+              <Icon name="help" />
+              Already signed up?&nbsp;<Link to="/">Login here</Link>
+              &nbsp;instead.
+            </Message>
+          </Grid.Column>
+        </Grid>
+      </Container>
     );
   }
 }
